1443f9d597629e32ee2e5daa7f1382bd18d1a251
AndrésDopico <adopico83@hotmail.com>
2025-10-26 21:38:42 +0100

    Añadir ejercicio mayor de tres números

---
+++ mayor_tres.py
@@ -0,0 +1,13 @@
+numero1 = int(input("Dime el primer número"))
+numero2 = int(input("Dime el segundo número"))
+numero3 = int(input("Dime el tercer número"))
+
+if numero1 > numero2 and numero1 > numero3:
+    print("El mayor es:", numero1)
+
+elif numero2 > numero1 and numero2 > numero3:
+    print("El mayor es:", numero2)
+
+elif numero3 > numero1 and numero3 > numero2:
+    print("El mayor es:", numero3)
+
