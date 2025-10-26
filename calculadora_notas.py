54e64725634b5cf78c5b98847e2011e5edf5e476
AndrésDopico <adopico83@hotmail.com>
2025-10-26 22:09:35 +0100

    Añadir calculadora de notas con media

---
+++ calculadora_notas.py
@@ -0,0 +1,11 @@
+nota1 = float(input("Dime la primera nota"))
+nota2 = float(input("Dime la segunda nota"))
+nota3 = float(input("Dime la tercera nota"))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 5:
+    print("Aprobado")
+
+else:
+    print("Suspenso")
